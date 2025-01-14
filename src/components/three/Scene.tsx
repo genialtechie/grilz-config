f4278229898f80ac492624fd8bc43131c471382e
genialtechie <only1hbo@icloud.com>
2025-01-14 13:51:09 -0500

    added orbit contol boundaries and camera

--- src/components/three/Scene.tsx
+++ src/components/three/Scene.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import GrillzModel from './GrillzModel';
 import { SceneProps } from '../../lib/types';
 
@@ -11,13 +11,21 @@ const Scene: React.FC<SceneProps> = ({
   toggleToothSelection,
 }) => {
   return (
-    <Canvas style={{ height: '500px', width: '500px' }}>
+    <Canvas style={{ height: '400px', width: '400px' }}>
+      <PerspectiveCamera
+        makeDefault
+        position={[0, 0, 10]}
+        fov={50}
+      />
       <ambientLight intensity={0.5} />
       <directionalLight
         position={[10, 10, 5]}
         intensity={1}
       />
-      <OrbitControls />
+      <OrbitControls
+        minDistance={50}
+        maxDistance={150}
+      />
       <GrillzModel
         customizations={customizations}
         selectedTeeth={selectedTeeth}
